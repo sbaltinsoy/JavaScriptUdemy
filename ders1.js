aad5124a6f0828c420acbedf22baedec582729d0
sbaltinsoy <serhatburakaltinsoy@gmail.com>
2021-03-17 00:26:10 +0300

    Js Giris

---
+++ ders1.js
@@ -0,0 +1,22 @@
+// ders degiskeni tanimlandi
+var ders = 'JavaScript';
+
+var isim = 'burak';
+
+// js de ; zorunlu degildir
+// K&R yazim stili
+function fonksiyon() {
+  return {
+    adi: 'Fatih',
+  };
+}
+
+// Allman Style
+/*
+function fonksiyon() {
+  return 
+  {
+    adi: 'Fatih',
+  };
+}
+*/
