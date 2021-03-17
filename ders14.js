a728af452753b743955f71a968cb820cf61eb948
sbaltinsoy <serhatburakaltinsoy@gmail.com>
2021-03-17 11:47:45 +0300

    element

--- ders14.js
+++ ders14.js
@@ -1,1 +1,26 @@
-// deneme vs code git ekleme
+// Element
+
+HTMLElement;
+// HTML ELEMENT MIDIR true
+document.body instanceof HTMLElement;
+// html document midir true
+document instanceof HTMLDocument;
+// html elementleri onceli acilir sonra kapanirlar attributleri vardir
+
+// <h1 class = "div1" id = "2">Selam<h1/>
+document.getElementById('2');
+
+var baslik = document.createElement('h1');
+baslik.innerText = 'Merhaba';
+document.body.appendChild(baslik);
+
+var paragraf = document.createElement('p');
+paragraf.innerText = 'nasilsin';
+// basliktan once yaz
+document.body.insertBefore(paragraf, baslik);
+
+baslik.setAttribute('style', 'color:red');
+baslik.style.color = 'orange';
+// burada yazdigimiz seyi dom uzerinden elemente erisip degistirmemizi sagliyor
+baslik.style.marginLeft = 10;
+baslik.style.marginRight = 10;
